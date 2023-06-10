f7db5d289b2afa5491aa82f04c8844b5e588d329
@AkbarDz <rabbaniakbar4@gmail.com>
2023-06-10 15:27:50 +0700

    responsive?

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -14,131 +12,140 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 49, 78, 207),
-      body: Stack(
-        children: [
-          Positioned(
-            top: 80,
-            left: 20,
-            child: Text('Details',style: TextStyle(
-              color: Colors.white,
-              fontSize: 25,
-            ),),
-          ),
-          Center(
-            child: Container(
-              padding: EdgeInsets.all(30),
-              
-              width: 250,
-              height: 450,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Meeting Summary',style: TextStyle(fontSize: 20),),
-                    ],
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    children: [
-                      Text('Pilihan Konseling',style: TextStyle(fontSize: 15),)
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  SingleChildScrollView(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: Text('jepi'),
-                        ),
-                      ],
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 80.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Details',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  Divider(
-                color: Colors.black,
-                thickness: 2,
-                indent: 5,
-                endIndent: 5,
-              ),
-              SizedBox(height: 10,),
-              Row(
-                    children: [
-                      Text('Pilihan Konseling',style: TextStyle(fontSize: 15),)
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  SingleChildScrollView(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: Text('jepi'),
-                        ),
-                      ],
+                    SizedBox(height: 40.0),
+                    Container(
+                      padding: EdgeInsets.all(30.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Meeting Summary',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          SizedBox(height: 20.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Pilihan Konseling', style: TextStyle(fontSize: 15.0)),
+                              SizedBox(height: 10.0),
+                              SingleChildScrollView(
+                                child: Wrap(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                                      child: Text('This is a longer sentence that should wrap instead of overflowing.'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Divider(
+                            color: Colors.black,
+                            thickness: 2.0,
+                            indent: 5.0,
+                            endIndent: 5.0,
+                          ),
+                          SizedBox(height: 10.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Pilihan Konseling', style: TextStyle(fontSize: 15.0)),
+                              SizedBox(height: 10.0),
+                              SingleChildScrollView(
+                                child: Wrap(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                                      child: Text('Another longer sentence that should wrap instead of overflowing.'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Divider(
+                            color: Colors.black,
+                            thickness: 2.0,
+                            indent: 5.0,
+                            endIndent: 5.0,
+                          ),
+                          SizedBox(height: 10.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Pilihan Konseling', style: TextStyle(fontSize: 15.0)),
+                              SizedBox(height: 10.0),
+                              SingleChildScrollView(
+                                child: Wrap(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                                      child: Text('A third longer sentence that should wrap instead of overflowing.'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Divider(
+                            color: Colors.black,
+                            thickness: 2.0,
+                            indent: 5.0,
+                            endIndent: 5.0,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  Divider(
-                color: Colors.black,
-                thickness: 2,
-                indent: 5,
-                endIndent: 5,
-              ),
-              SizedBox(height: 10,),
-              Row(
-                    children: [
-                      Text('Pilihan Konseling',style: TextStyle(fontSize: 15),)
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  SingleChildScrollView(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: Text('jepi'),
+                    SizedBox(height: 40.0),
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.symmetric(horizontal: 35.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size.fromHeight(40.0),
+                          backgroundColor: Color.fromARGB(255, 255, 195, 3),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
-                      ],
+                        child: Text('SUBMIT'),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10,),
-                  Divider(
-                color: Colors.black,
-                thickness: 2,
-                indent: 5,
-                endIndent: 5,
-              ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 80,
-            left: 30,
-            child: Container(
-              width: 300,
-              height: 50,
-              padding: EdgeInsets.fromLTRB(35, 10, 35, 0),
-              child: Center(
-                child: ElevatedButton(
-                          onPressed: () {
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size.fromHeight(40),
-                              backgroundColor: Color.fromARGB(255, 255, 195, 3),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                          child: Text('SUBMIT')),
-              ),
-            ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,5 @@
 import 'package:bk_flutter/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,103 +10,127 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        body: Stack(
-          children: [
-            Positioned(
-                child: Container(
-              height: 350,
-              color: Color.fromARGB(255, 49, 78, 207),
-            )),
-            Positioned(
-                top: 80,
-                left: 20,
-                child: Image.asset(
-                  'assets/images/Logo.png',
-                  scale: 2,
-                )),
-            Positioned(
-                top: 110,
-                right: 20,
-                child: Image.asset(
-                  'assets/images/loginimage.png',
-                  height: 150,
-                )),
-            Positioned(
-                top: 210,
-                left: 20,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Stack(
                   children: [
-                    Text(
-                      'SIGN IN',
-                      style: TextStyle(color: Colors.white, fontSize: 36),
+                    Positioned(
+                      child: Container(
+                        height: 350,
+                        color: Color.fromARGB(255, 49, 78, 207),
+                      ),
                     ),
-                    Text(
-                      'To Your Account',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w100),
-                    )
-                  ],
-                )),
-            Form(
-                key: _formKey,
-                child: ListView(
-                  children: [
-                    SizedBox(
-                      height: 350,
+                    Positioned(
+                      top: 80,
+                      left: 20,
+                      child: Image.asset(
+                        'assets/images/Logo.png',
+                        scale: 2,
+                      ),
                     ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(35, 0, 35, 20),
-                      child: TextFormField(
-                        style: TextStyle(fontSize: 10),
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 3, color: Colors.black),
-                              borderRadius: BorderRadius.circular(15)),
-                          hintText: 'Enter Your Email',
-                          labelText: 'Email',
+                    Positioned(
+                      top: 110,
+                      right: 20,
+                      child: Image.asset(
+                        'assets/images/Coffee.png',
+                        height: 150,
+                      ),
+                    ),
+                    Positioned(
+                      top: 210,
+                      left: 20,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'SIGN IN',
+                            style: TextStyle(color: Colors.white, fontSize: 36),
+                          ),
+                          Text(
+                            'To Your Account',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(35, 370, 35, 0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              style: TextStyle(fontSize: 10),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(width: 3, color: Colors.black),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                hintText: 'Enter Your Email',
+                                labelText: 'Email',
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              style: TextStyle(fontSize: 10),
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(width: 3, color: Colors.black),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                hintText: 'Enter Your Password',
+                                labelText: 'Password',
+                              ),
+                            ),
+                            SizedBox(height: 50),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(builder: (context) {
+                                    return HistoryPage();
+                                  }),
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 255, 195, 3),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 40, vertical: 15),
+                                child: Text('LOGIN'),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(35, 0, 35, 50),
-                      child: TextFormField(
-                        style: TextStyle(fontSize: 10),
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 3, color: Colors.black),
-                              borderRadius: BorderRadius.circular(15)),
-                          hintText: 'Enter Your Password',
-                          labelText: 'Password',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(45, 0, 45, 0),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(context,
-                                MaterialPageRoute(builder: (context) {
-                              return HistoryPage();
-                            }));
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromARGB(255, 255, 195, 3),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50))),
-                          child: Text('LOGIN')),
-                    )
                   ],
-                ))
-          ],
-        ));
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
